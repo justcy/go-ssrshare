3b46447f15df83d2fe3a37c7aeb588fbd52516d1
chenyu <chenyu@2xi.com>
2019-05-09 12:49:32 +0800

    更新项目

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM alpine:latest
 MAINTAINER justcy <justxcy@gmail.com>
 
+RUN apk update && apk add ca-certificates && rm -rf /var/cache/apk/*
+
 ADD ./dist/go-ssrshare /bin
 
 RUN \
--- ssr.go
+++ ssr.go
@@ -9,13 +9,12 @@ import (
 	"strings"
 )
 
-func main(){
+func main() {
 
 	http.HandleFunc("/", ssr)
 	http.HandleFunc("/dxxzst", dxxzst)
 
 	http.ListenAndServe(":8922", nil)
-
 
 	//str := httpGet("https://github.com/dxxzst/Free-SS-SSR")
 	//reg := httpRegex(str)
@@ -24,25 +23,25 @@ func main(){
 	//fmt.Println(base64.StdEncoding.EncodeToString(strbytes))
 }
 
-func ssr(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "欢迎使用免费ssr订阅服务")
+func ssr(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "欢迎使用免费ssr订阅服务1")
 }
 
-func dxxzst(w http.ResponseWriter, r *http.Request){
-	str,err := httpGet("https://github.com/dxxzst/Free-SS-SSR")
-	if err != nil{
-		fmt.Sprint(w,err)
+func dxxzst(w http.ResponseWriter, r *http.Request) {
+	str, err := httpGet("https://github.com/dxxzst/Free-SS-SSR")
+	if err != nil {
+		fmt.Sprint(w, err)
 	}
-	reg,err:= httpRegex(str)
+	reg, err := httpRegex(str)
 	result := strings.Join(reg, "\n")
 	strbytes := []byte(result)
 	fmt.Fprintf(w, base64.StdEncoding.EncodeToString(strbytes))
 }
 
-func httpGet(url string) (str string ,err error){
-	resp,err := http.Get(url)
+func httpGet(url string) (str string, err error) {
+	resp, err := http.Get(url)
 	if err != nil {
-		return "",err
+		return "", err
 	}
 
 	defer resp.Body.Close()
@@ -52,11 +51,11 @@ func httpGet(url string) (str string ,err error){
 	if err != nil {
 
 		fmt.Println(err)
-		return "",err
+		return "", err
 	}
-	return string(body),err
+	return string(body), err
 }
-func httpRegex(str string) (results []string ,err error) {
+func httpRegex(str string) (results []string, err error) {
 	//正则表达式，有点菜，只会(.*?)
 	regex := "<td align=\"left\">ssr:(.*?)</td>"
 
@@ -64,12 +63,12 @@ func httpRegex(str string) (results []string ,err error) {
 
 	dataS := reg.FindAllSubmatch([]byte(str), -1)
 
-	results = make([]string,0)
+	results = make([]string, 0)
 
-	if dataS != nil{
-		for _,v := range dataS {
-			results = append(results,string("ssr:"+string(v[1])))
+	if dataS != nil {
+		for _, v := range dataS {
+			results = append(results, string("ssr:"+string(v[1])))
 		}
 	}
-	return results,err
+	return results, err
 }
